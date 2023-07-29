2fe8f87e6fa4349bd2fb38e6933037109be2918d
Vladimir <vovachempogir@mail.ru>
2023-07-29 20:42:36 +0700

    added updates

--- src/main/java/com/teamwork/telegrambotanimalshelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/teamwork/telegrambotanimalshelter/listener/TelegramBotUpdatesListener.java
@@ -2,15 +2,35 @@ package com.teamwork.telegrambotanimalshelter.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import com.teamwork.telegrambotanimalshelter.model.owners.Owner;
+import com.teamwork.telegrambotanimalshelter.service.OwnerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
 
+@Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-    @Autowired
+
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
     private TelegramBot telegramBot;
+
+    private final OwnerService ownerService;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, OwnerService ownerService) {
+        this.telegramBot = telegramBot;
+        this.ownerService = ownerService;
+    }
 
     @PostConstruct
     public void init() {
@@ -24,6 +44,36 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
      */
     @Override
     public int process(List<Update> list) {
-        return 0;
+        try {
+            list.forEach(update -> {
+                logger.info("Processing update: {}", update);
+                Message message = update.message();
+                Long chatId = message.chat().id();
+                String text = message.text();
+                if ("/start".equals(text)) {
+                    sendMessage(chatId,
+                            " Здравствуйте, здесь вы сможете узнать о приюте,как его забрать и ухаживать за ним. ");
+                }
+                if ("/dogShelterInfo".equals(text)) {
+                    sendMessage(chatId,
+                            "Это приют для собак. Здесь вы можете о часах работы и местонахождении приюта");
+                }
+                if ("/catShelterInfo".equals(text)) {
+                    sendMessage(chatId,
+                            "Это приют для кошек. Здесь вы можете о часах работы и местонахождении приюта");
+                }
+            });
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private void sendMessage(Long chatId, String message) {
+        SendMessage sendMessage = new SendMessage(chatId,message);
+        SendResponse sendResponse = telegramBot.execute(sendMessage);
+        if (!sendResponse.isOk()) {
+            logger.error("Error during sending message: {}", sendResponse.description());
+        }
     }
 }
--- src/main/java/com/teamwork/telegrambotanimalshelter/model/animals/Animal.java
+++ src/main/java/com/teamwork/telegrambotanimalshelter/model/animals/Animal.java
@@ -1,6 +1,7 @@
 package com.teamwork.telegrambotanimalshelter.model.animals;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.teamwork.telegrambotanimalshelter.exceptions.IncorrectArgumentException;
 import com.teamwork.telegrambotanimalshelter.model.enums.AnimalType;
 import com.teamwork.telegrambotanimalshelter.model.owners.Owner;
 import lombok.AllArgsConstructor;
@@ -51,6 +52,12 @@ public class Animal {
         this.name = name;
         this.age = age;
         this.owner = owner;
+    }
+
+    public void setOwner(Owner owner) {
+        if(owner.getOwnerType().equals(this.getAnimalType())) {
+            this.owner = owner;
+        } else throw new IncorrectArgumentException("У одного владельца могут быть только однотипные животные");
     }
 
     @Override
