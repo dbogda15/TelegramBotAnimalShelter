7cdf30945a2ed7159b86c8804a6480ad5ef3d06e
Vladimir <vovachempogir@mail.ru>
2023-08-08 14:24:30 +0700

    added buttons model

--- src/main/java/com/teamwork/telegrambotanimalshelter/replymarkup/ReplyMarkUp.java
+++ src/main/java/com/teamwork/telegrambotanimalshelter/replymarkup/ReplyMarkUp.java
@@ -63,6 +63,73 @@ public class ReplyMarkUp {
         returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, "Информация о собачьем приюте");
     }
 
+    public void menuCat(long chatId) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton(Keyboard.REQUIRED_DOCUMENTS),
+                new KeyboardButton(Keyboard.LIST_OF_REASONS)
+        );
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.RECOMMENDATIONS_FOR_CATS),
+                new KeyboardButton(Keyboard.CALL_A_VOLUNTEER),
+                new KeyboardButton(Keyboard.CONTACT_DETAILS)
+        );
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.MAIN_MENU)
+        );
+        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, "Все о кошках");
+    }
+
+    public void menuDog(long chatId) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton(Keyboard.REQUIRED_DOCUMENTS),
+                new KeyboardButton(Keyboard.LIST_OF_REASONS)
+        );
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.RECOMMENDATIONS_FOR_DOGS),
+                new KeyboardButton(Keyboard.CALL_A_VOLUNTEER),
+                new KeyboardButton(Keyboard.CONTACT_DETAILS)
+        );
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.MAIN_MENU)
+        );
+        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, "Все о собаках");
+    }
+
+    public void rulesForDogs(long chatId) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton(Keyboard.RULES_FOR_DATING_A_DOG),
+                new KeyboardButton(Keyboard.DOG_CARRIAGE),
+                new KeyboardButton(Keyboard.PUPPY_SETUP)
+        );
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.ADULT_DOG_SETUP),
+                new KeyboardButton(Keyboard.ARRANGEMENT_OF_DOG_WITH_DISABILITIES)
+        );
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.DOG_HANDLERS_ADVICE),
+                new KeyboardButton(Keyboard.PROVEN_CYNOLOGISTS));
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.BACK_TO_ALL_ABOUT_DOGS)
+        );
+        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, Keyboard.RECOMMENDATIONS_FOR_DOGS);
+    }
+
+    public void rulesForCats(long chatId) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton(Keyboard.RULES_FOR_DATING_A_CAT),
+                new KeyboardButton(Keyboard.CAT_CARRIAGE)
+        );
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.ADULT_CAT_SETUP),
+                new KeyboardButton(Keyboard.KITTEN_SETUP),
+                new KeyboardButton(Keyboard.ARRANGEMENT_OF_CAT_WITH_DISABILITIES)
+        );
+        replyKeyboardMarkup.addRow(
+                new KeyboardButton(Keyboard.BACK_TO_ALL_ABOUT_CATS)
+        );
+        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, Keyboard.RECOMMENDATIONS_FOR_CATS);
+    }
+
     public void returnResponseReplyKeyboardMarkup(ReplyKeyboardMarkup replyKeyboardMarkup, Long chatId, String text) {
         replyKeyboardMarkup.resizeKeyboard(true);
         replyKeyboardMarkup.oneTimeKeyboard(false);
