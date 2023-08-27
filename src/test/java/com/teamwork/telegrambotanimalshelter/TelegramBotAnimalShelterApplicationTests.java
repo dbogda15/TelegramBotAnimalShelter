d93677bcfd9c39e9eda2c5569b9e4c4e6f0fa915
Vladimir <vovachempogir@mail.ru>
2023-08-27 09:42:40 +0700

    Кнопки сделаны

---
+++ src/test/java/com/teamwork/telegrambotanimalshelter/TelegramBotAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamwork.telegrambotanimalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotAnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
